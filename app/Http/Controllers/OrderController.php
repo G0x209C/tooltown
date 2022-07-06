c8a87a577191275594a10cdd572ff1a23c989f5e
G0x209C <g@collette-online.nl>
2022-07-06 13:39:12 +0200

    Extended definition in controller

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -4,13 +4,27 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+
 class OrderController extends Controller
 {
-    public function index(){}
-    public function create(){}
-    public function store(){}
-    public function show($request){
+    public function index(Request $request){
+
+        //dd($request->user());
+        $data = \App\Models\Order::where('user_id',$request->user()->id)->get();
+        return view('account.orders', $data);
 
     }
-    public function
+    public function create(Request $request){
+        // show create order page.
+        return view('shop.order');
+    }
+    // store order (this is done before payment. Upon payment, the order is updated to reflect payment.)
+    public function store(){}
+    // show the specifics of one order
+    public function show(Request $request){
+
+    }
+    public function edit(){}
+    public function update(){}
+    public function destroy(){}
 }
